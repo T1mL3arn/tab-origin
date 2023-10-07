34b752c581b4f81174d51a0e36684dc34bb3f7dd
Tim <tim@noreplay.com>
2023-10-08 03:17:56 +1000

    add simple popup and code to open it

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
+const SHOW_POPUP = 'show-popup'
+const GO_TO_TAB_IF_OPEN = 'go-to-tab-if-open'
+const GO_TO_TAB = 'go-to-tab'
+
 // TODO: port to browser api
 const api = chrome;
 
 const activeTabKey = "activeTab";
+const actionTypeKey = 'extension-action-type'
 
 // Return the last element in an array.
 // If the array is empty, return null.
@@ -11,8 +16,48 @@ function last(array) {
     return array[array.length - 1];
 }
 
+/* 
+    opening popup requires user gesture,
+    but using await in action.onClicked()
+    removes "user gesture" from the browser's point of view,
+    so I have to subscribe to storage changes and
+    prepare popup beforehand
+*/
+api.storage.local.onChanged.addListener(changes => {
+    const actionType = changes[actionTypeKey]
+    // looking only for "action type" changes
+    if (actionType && actionType.newValue === SHOW_POPUP)
+        api.action.setPopup({ popup: '/popup/popup.html' })
+    else 
+        api.action.setPopup({ popup: '' })
+})
+ 
 // Open the origin url of the given tab.
-function openTabOrigin(tab) {
+async function openTabOrigin(tab) {
+    const action = await api.storage.local.get({ [actionTypeKey]: GO_TO_TAB }).then( d => d[actionTypeKey])
+    console.log('chosen action:', action)
+
+    switch (action) {
+        case SHOW_POPUP:
+
+            api.action.openPopup()
+            console.log('opening popup...');
+            return;
+
+        case GO_TO_TAB_IF_OPEN:
+
+            break;
+
+        case GO_TO_TAB:
+        default:
+
+            api.action.setPopup({ popup: null })
+            console.log('default action');
+            break;
+    }
+
+    return;
+      
     const id = tab.id.toString();
     api.storage.local.get(id, function(result) {
         const result_stack = result[id] || [];
